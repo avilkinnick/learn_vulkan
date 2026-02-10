f8ff28cc98dc4f76deb07e899da4d829d8c90f2a
avilkinnick <avilkin.nick@gmail.com>
2026-02-10 19:45:05 +0300

    Initialize volk

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
 #include <volk.h>
 
+#include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
+    if (volkInitialize() != VK_SUCCESS)
+    {
+        fputs("Failed to initialize volk\n", stderr);
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
