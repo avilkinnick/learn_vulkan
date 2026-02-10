30bab6144a5da17b1aef7f69913d4184ca523261
avilkinnick <avilkin.nick@gmail.com>
2026-02-10 19:50:37 +0300

    Get Vulkan instance version

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
 #include <volk.h>
 
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+static void print_vk_version(const uint32_t version)
+{
+    printf("%u.%u.%u.%u", VK_API_VERSION_VARIANT(version),
+                          VK_API_VERSION_MAJOR(version),
+                          VK_API_VERSION_MINOR(version),
+                          VK_API_VERSION_PATCH(version));
+}
 
 int main(void)
 {
@@ -10,6 +19,12 @@ int main(void)
         fputs("Failed to initialize volk\n", stderr);
         return EXIT_FAILURE;
     }
+
+    uint32_t vk_instance_version;
+    vkEnumerateInstanceVersion(&vk_instance_version);
+    printf("vk_instance_version: ");
+    print_vk_version(vk_instance_version);
+    printf("\n");
 
     return EXIT_SUCCESS;
 }
